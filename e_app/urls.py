5cab0e501a189cbee988f0aba4a0b4d65e3262aa
Koushik Mallik <koushikmallik001@gmail.com>
2024-12-12 23:17:21 +0530

    Fixed project structure added black and flake8 for formatting

--- e_app/urls.py
+++ e_app/urls.py
@@ -3,5 +3,5 @@ from .views import TestView
 
 
 urlpatterns = [
-    path('endpoint/', TestView.as_view(), name='test_endpoint'),
+    path("endpoint/", TestView.as_view(), name="test_endpoint"),
 ]
