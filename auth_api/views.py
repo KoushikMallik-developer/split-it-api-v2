dace7f4ecabeca51a72f2fa1cd3af361661df5c1
Koushik Mallik <koushikmallik001@gmail.com>
2024-12-13 00:30:43 +0530

    Added PostgreSQL database hosted at Render Added User and required abstract models Added.env

--- auth_api/views.py
+++ auth_api/views.py
@@ -9,16 +9,8 @@ from rest_framework.views import APIView
 class EnvRead(APIView):
     def get(self, _):
         load_dotenv()
-        default_data = {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USERNAME"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
-        }
         return Response(
-            data={"data": default_data, "errorMessage": None},
+            data={"data": None, "errorMessage": None},
             status=status.HTTP_200_OK,
             content_type="application/json",
         )
