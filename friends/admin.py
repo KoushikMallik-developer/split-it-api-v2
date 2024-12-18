948cfc7903db31f4251deb145dea69ed3a7ceda7
animeshrick <animeshece198@gmail.com>
2024-12-19 01:22:49 +0530

    update models name

--- friends/admin.py
+++ friends/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from friends.models.friend_model import Friend
-from friends.models.friend_request_model import FriendRequest
+from friends.models.friend import Friend
+from friends.models.friend_request import FriendRequest
 
 admin.site.register(Friend)
 admin.site.register(FriendRequest)
