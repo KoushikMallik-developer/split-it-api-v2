e4a9308a423688a9074a0c8a4dacc43add4d4142
animeshrick <animeshece198@gmail.com>
2025-01-05 16:33:53 +0530

    add group - serializer is update and modify

--- groups/serializers/group_serializer.py
+++ groups/serializers/group_serializer.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 from auth_api.auth_exceptions.user_exceptions import UserNotFoundError
 from auth_api.export_types.validation_types.validation_result import ValidationResult
 from auth_api.models.user_models.user import User
-from auth_api.services.helpers import validate_email, validate_user_email
+from auth_api.services.helpers import validate_user_email
 from friends.friend_exceptions.friend_exceptions import FriendNotFoundError
 from friends.models.friend import Friend
 from groups.export_types.create_group import CreateGroupRequestType
@@ -28,11 +28,22 @@ class GroupSerializer(serializers.ModelSerializer):
             raise ValueError(f"A group with the name '{name}' already exists.")
 
         if members:
-            for member in members:
-                if not User.objects.filter(email=member).exists():
-                    raise UserNotFoundError(msg=f"User '{member}' does not exist.")
-                if not Friend.objects.filter(user2__id=uid, user1__id=User.objects.get(email=member).id).exists():
-                    raise FriendNotFoundError(msg=f"'{member}' is not your friend.")
+            for member_email in members:
+
+                # check if the member is a valid email
+                if member_email and member_email != "" and isinstance(member_email, str):
+                    validation_result_email: ValidationResult = validate_user_email(member_email)
+                    is_validated_email = validation_result_email.is_validated
+                    if not is_validated_email:
+                        raise serializers.ValidationError(detail=validation_result_email.error)
+
+                # check if the member is a valid user
+                if not User.objects.filter(email=member_email).exists():
+                    raise UserNotFoundError(msg=f"User '{member_email}' does not exist.")
+
+                # check if the member is a friend
+                if not Friend.objects.filter(user2__id=uid, user1__id=User.objects.get(email=member_email).id).exists():
+                    raise FriendNotFoundError(msg=f"'{member_email}' is not your friend.")
 
         return True
 
@@ -53,20 +64,16 @@ class GroupSerializer(serializers.ModelSerializer):
                 list_members.append(creator)
             else:
                 for member_email in members:
-                    if member_email and member_email != "" and isinstance(member_email, str):
-                        validation_result_email: ValidationResult = validate_user_email(member_email)
-                        is_validated_email = validation_result_email.is_validated
-                        if not is_validated_email:
-                            raise serializers.ValidationError(detail=validation_result_email.error)
+                    user_friends: User = User.objects.get(email=member_email)
+                    list_members.append(user_friends)
 
-                        user_friends: User = User.objects.get(email=member_email)
-                        list_members.append(user_friends)
-
-            list_members.insert(0, creator)
+            if creator not in list_members:
+                list_members.insert(0, creator)
 
             group = Group.objects.create(name=name, image=image, creator=creator)
 
             group.members.set(list_members)
+
             if group:
                 group.save()
                 return group
