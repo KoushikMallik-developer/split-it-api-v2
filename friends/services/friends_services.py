f23787e1c198fc5937716c72c9991cd48eb15372
animeshrick <animeshece198@gmail.com>
2025-01-15 23:31:36 +0530

    new endpoint for search friend

--- friends/services/friends_services.py
+++ friends/services/friends_services.py
@@ -20,6 +20,9 @@ from friends.export_types.request_data_types.remove_friend import RemoveFriendTy
 from friends.export_types.request_data_types.remove_friend_requeset import (
     RemoveFriendRequestType,
 )
+from friends.export_types.request_data_types.search_friend import (
+    SearchFriendRequestType,
+)
 from friends.friend_exceptions.friend_exceptions import (
     FriendRequestNotSentError,
     FriendRequestNotAcceptedError,
@@ -52,6 +55,24 @@ class UseFriendServices:
                 friend_list=all_friend, user_id=user_id
             )
             return all_friend_details
+        else:
+            return None
+
+    @staticmethod
+    def get_searched_friends(
+        request_data: SearchFriendRequestType, user_id: str
+    ) -> Optional[ExportFriendList]:
+        friends = Friend.objects.filter(
+            Q(user1__fname__icontains=request_data.keyword)
+            | Q(user1__lname__icontains=request_data.keyword)
+            | Q(user1__email__icontains=request_data.keyword)
+            | Q(user2__fname__icontains=request_data.keyword)
+            | Q(user2__lname__icontains=request_data.keyword)
+            | Q(user2__email__icontains=request_data.keyword)
+        )
+        if friends:
+            all_friend = [ExportFriend(**friend.model_to_dict()) for friend in friends]
+            return ExportFriendList(friend_list=all_friend, user_id=user_id)
         else:
             return None
 
