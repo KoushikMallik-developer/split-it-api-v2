3b4f090fd6364d41c63aa9ba93e705beef062fc5
animeshrick <animeshece198@gmail.com>
2025-01-25 20:16:15 +0530

    create group search

--- groups/urls.py
+++ groups/urls.py
@@ -4,6 +4,7 @@ from groups.view.add_members import AddMemberView
 from groups.view.create_group import CreateGroupView
 from groups.view.get_all_groups import GetAllGroupView
 from groups.view.remove_group import RemoveGroupView
+from groups.view.search_group import SearchGroupView
 from groups.view.update_group import UpdateGroupView
 
 urlpatterns = [
@@ -11,5 +12,6 @@ urlpatterns = [
     path("add-member", AddMemberView.as_view(), name="Add-Member"),
     path("update-group", UpdateGroupView.as_view(), name="Update-Group"),
     path("remove-group", RemoveGroupView.as_view(), name="Remove-Group"),
+    path("search-group", SearchGroupView.as_view(), name="Search-Group"),
     path("get-all-group", GetAllGroupView.as_view(), name="Get-all-Group"),
 ]
