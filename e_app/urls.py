9da377035d1f14ea08a761e8c07950e4deb558a2
Koushik Mallik <koushikmallik001@gmail.com>
2025-01-26 02:24:17 +0530

    added get group expenses added get all expenses added remove expense

--- e_app/urls.py
+++ e_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from e_app.views.add_expense import AddExpense
+from e_app.views.get_all_groups import GetAllExpenses
+from e_app.views.get_group_expenses import GetGroupExpenses
+from e_app.views.remove_expense import RemoveExpense
 
 urlpatterns = [
     path("add-expense/", AddExpense.as_view(), name="add-expense"),
+    path("remove-expense/", RemoveExpense.as_view(), name="remove-expense"),
+    path("get-group-expenses/", GetGroupExpenses.as_view(), name="get-group-expense"),
+    path("get-all-expenses/", GetAllExpenses.as_view(), name="get-all-expense"),
 ]
--- e_app/views/get_group_expenses.py
+++ e_app/views/get_group_expenses.py
@@ -6,32 +6,28 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import TokenError
 from auth_api.services.handlers.exception_handlers import ExceptionHandler
 from auth_api.services.helpers import decode_jwt_token, validate_user_uid
-from e_app.export_types.request_data_types.add_expense_request_type import (
-    AddExpenseRequestType,
+from e_app.export_types.request_data_types.get_group_expense_request_type import (
+    GetGroupExpenseRequestType,
 )
 from e_app.services.expense_services import ExpenseService
 
 
-class AddExpense(APIView):
+class GetGroupExpenses(APIView):
     renderer_classes = [JSONRenderer]
 
-    def post(self, request: Request):
+    def get(self, request: Request):
         try:
             user_id = decode_jwt_token(request=request)
             if validate_user_uid(uid=user_id).is_validated:
-                result = ExpenseService().add_expense_service(
-                    data=AddExpenseRequestType(**request.data)
+                result = ExpenseService().get_group_expense_service(
+                    data=GetGroupExpenseRequestType(**request.data), uid=user_id
                 )
                 return Response(
                     data={
-                        "message": (
-                            result.get("message")
-                            if result.get("message")
-                            else "Expense added successfully"
-                        ),
-                        "data": result.get("expense").model_dump(),
+                        "message": "Expenses fetched successfully",
+                        "data": result,
                     },
-                    status=status.HTTP_201_CREATED,
+                    status=status.HTTP_200_OK,
                     content_type="application/json",
                 )
 
